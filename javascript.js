ef495f339adf76f6f46fa7926ec37b31efb68a5a
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-01 15:07:02 -0400

    feat: Added basic func framework

--- javascript.js
+++ javascript.js
@@ -1,8 +1,31 @@
 loadEventListeners();
 
-/* Prevent default on submit for player names form */
+/* Prevent default on submit for player names form
+   Loads event listeners on page load */
 function loadEventListeners() {
   document.addEventListener("submit", (e) => {
     e.preventDefault();
   });
 }
+
+/* Store game board data */
+function setGameBoard() {
+  const gameBoardData = [];
+
+  gameLogic(gameBoardData);
+}
+
+/* Player constructor */
+function Player(name, side) {
+  this.name = name;
+  this.side = side;
+}
+
+/* Game logic  to determine winner*/
+function gameLogic(gameBoardData) {}
+
+/* Start new game button */
+function startNewGame() {}
+
+/* Display game winner */
+function displayGameWinner() {}
