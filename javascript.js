66f23fc45723c4a1cc035b3939740c0735e9faff
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-02 15:52:33 -0400

    Added player turn functionality in progress.

--- javascript.js
+++ javascript.js
@@ -27,11 +27,13 @@ function setPlayerData() {
   const playerX = {
     name: "Player X",
     side: "X",
+    turnCounter: 0,
   };
 
   const playerO = {
     name: "Player O",
     side: "O",
+    turnCounter: 0,
   };
   // If no entered names use default else use names from form.
   if (
@@ -52,11 +54,53 @@ function setPlayerData() {
 /* Game logic  to determine winner*/
 function gameLogic(cell) {
   const player = setPlayerData();
-  if (cell.value === undefined) {
-    cell.textContent = "X";
-  } else {
-    // Do Nothing
+  // Determine next players turn.
+  let currentPlayerTurn = determinePlayerTurn(player);
+  // Determines if turn is complete.
+  let isGameComplete = gameProgress();
+
+  // Determines if X or O should be placed on board then ends turn.
+  // Game continues.
+  while (isGameComplete === false) {
+    if (cell.textContent === "") {
+      if (currentPlayerTurn === "X") {
+        cell.textContent = "X";
+        currentPlayerTurn = determinePlayerTurn(player);
+      } else {
+        cell.textContent = "O";
+        currentPlayerTurn = determinePlayerTurn(player);
+      }
+    } else {
+      console.log("Turn is complete.");
+      // Continue the game - Start here next day.
+      break;
+    }
   }
+}
+
+/* Determine players turn. */
+function determinePlayerTurn(player) {
+  // Count if it is the player turn. If X counter is higher it is O's turn
+  // if not it is X's turn.
+  let playerTurn;
+
+  if (player.playerX.turnCounter <= player.playerO.turnCounter) {
+    console.log("Player X's turn.");
+    player.playerX.turnCounter++;
+    playerTurn = "X";
+  } else {
+    console.log("Player O's turn.");
+    player.playerO.turnCounter++;
+    playerTurn = "O";
+  }
+  return playerTurn;
+}
+
+/* Game in progress (used to determine when game is ended) */
+function gameProgress() {
+  let isGameComplete = false;
+
+  return isGameComplete;
 }
 
 /* Start new game button */
